d726c9312d84778407b6fbc92281256f6cd685f5
Daulet <baltashdaulet3@gmail.com>
2025-02-18 09:26:26 +0500

    first commit

---
+++ src/MUDController.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class MUDController {
+
+        private final Player player;
+        private boolean running;
+
+
+        public MUDController(Player player, Player player1) {
+            this.player = player1;
+        }
+
+
+        public void runGameLoop() {
+
+        }
+
+
+        public void handleInput(String input) {
+                }
+
+
+        private void lookAround() {
+        }
+
+
+        private void move(String direction) {
+
+        }
+
+
+        private void pickUp(String arg) {
+
+        }
+
+        private void checkInventory() {
+
+        }
+
+        private void showHelp() {
+        }
+
+
+
+}
